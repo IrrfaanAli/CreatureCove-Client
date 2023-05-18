b6b2cc080d0f8f9a6773eb27360d83eca1b6aadd
Irfan Ali <alimohammadirfan7@gmail.com>
2023-05-18 17:31:11 +0600

    shop by category section added

--- src/component/Subtabs.jsx
+++ src/component/Subtabs.jsx
@@ -27,26 +27,29 @@ const Subtabs = () => {
     const unicorn = toy.filter(toys => toys.category === unicornCategory)
 
     return (
+        <>
+         <h1 className='text-center text-4xl font-bold p-7'>Shop by category</h1>
+         <div  className="">
         <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-            <TabList className=" flex m-3 justify-between">
-                <Tab> <button>Dog</button></Tab>
-                <Tab>Cat</Tab>
-                <Tab>Dinosur</Tab>
-                <Tab>Unicorn</Tab>
+            <TabList className=" flex m-3 justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                <Tab> <button className='btn btn-wide'>Dog</button></Tab>
+                <Tab> <button className='btn btn-wide'>Cat</button></Tab>
+                <Tab><button className='btn btn-wide'>Dinosur</button></Tab>
+                <Tab><button className='btn btn-wide'>Unicorn</button></Tab>
             </TabList>
-            <TabPanel>
+            <TabPanel className="flex justify-center gap-28 p-7">
                 {
 
                     dog?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
                 }
             </TabPanel>
-            <TabPanel>
+            <TabPanel className="flex justify-center gap-28 p-7">
                 {
 
                     cat?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
                 }
             </TabPanel>
-            <TabPanel>
+            <TabPanel className="flex justify-center gap-28  p-7">
                 {
 
                     dinosaur?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
@@ -59,6 +62,8 @@ const Subtabs = () => {
                 }
             </TabPanel>
         </Tabs>
+        </div>
+        </>
     );
 
 };
