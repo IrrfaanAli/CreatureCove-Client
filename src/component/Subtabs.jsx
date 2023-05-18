0ffc9dab00dbac8a3b69a30ff203c5f690b5fbab
Irfan Ali <alimohammadirfan7@gmail.com>
2023-05-18 16:57:00 +0600

    tab section added

--- src/component/Subtabs.jsx
+++ src/component/Subtabs.jsx
@@ -1,22 +1,66 @@
+import { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-const Subtabs = () => {
-    return (
-        <div>
-            <Tabs>
-                <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
-                </TabList>
+import Card from './Card';
 
-                <TabPanel>
-                    <h2>Any content 1</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                </TabPanel>
-            </Tabs>
-        </div>
+
+
+const Subtabs = () => {
+
+    const [toy, setToys] = useState([]);
+    const [tabIndex, setTabIndex] = useState(0);
+
+    let dogCategory = "a";
+    let catCategory = "b";
+    let dinosaurCategory = "c";
+    let unicornCategory = "d";
+
+    useEffect(() => {
+        fetch('/chefs.json')
+            .then(res => res.json())
+            .then(data => setToys(data));
+
+    }, [])
+
+    const dog = toy.filter(toys => toys.category === dogCategory)
+    const cat = toy.filter(toys => toys.category === catCategory)
+    const dinosaur = toy.filter(toys => toys.category === dinosaurCategory)
+    const unicorn = toy.filter(toys => toys.category === unicornCategory)
+
+    return (
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+            <TabList className=" flex m-3 justify-between">
+                <Tab> <button>Dog</button></Tab>
+                <Tab>Cat</Tab>
+                <Tab>Dinosur</Tab>
+                <Tab>Unicorn</Tab>
+            </TabList>
+            <TabPanel>
+                {
+
+                    dog?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
+                }
+            </TabPanel>
+            <TabPanel>
+                {
+
+                    cat?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
+                }
+            </TabPanel>
+            <TabPanel>
+                {
+
+                    dinosaur?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
+                }
+            </TabPanel>
+            <TabPanel>
+                {
+
+                    unicorn?.map((toys) => <Card key={toys.id} toys={toys} ></Card>)
+                }
+            </TabPanel>
+        </Tabs>
     );
+
 };
 
 export default Subtabs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
+
 import Banner from "../component/Banner";
 import Footer from "../component/Footer";
 import Gallery from "../component/Gallery";
 import Navbar from "../component/Navbar";
+import Subtabs from "../component/Subtabs";
 
 
 const Home = () => {
+ 
+ 
     return (
         <div>
            <Navbar></Navbar>
             <Banner></Banner>
             <Gallery></Gallery>
+            <Subtabs></Subtabs>
            <Footer></Footer>          
         </div>
     );
